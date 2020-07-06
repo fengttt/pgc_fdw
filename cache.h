fa02cc358315b432d1e283b8cd1b3cc160ffce68
Feng Tian <fengttt@gmail.com>
2020-07-06 19:02:40 +0000

    save

--- cache.h
+++ cache.h
@@ -8,6 +8,7 @@
 #define PGC_FDW_CACHE_H
 
 #include <stdint.h>
+#include <openssl/sha.h>
 
 #define UNUSED(x) (void)(x)
 
@@ -39,8 +40,38 @@ typedef struct qry_key_t {
 typedef struct qry_val_t {
 	int64_t ts;
 	int32_t status;
+	int32_t txtsz;
 	char qrytxt[1];
 } qry_val_t;
+
+static inline void qry_key_init(qry_key_t *k, const char *sha) {
+	memcpy(k->PREFIX, "PGCQ", 4);
+	if (sha) {
+		memcpy(k->SHA, sha, 20);
+	} else {
+		memset(k->SHA, 0, 20);
+	}
+}
+
+static inline void qry_key_build(qry_key_t *k, const char* qry) {
+	memcpy(k->PREFIX, "PGCQ", 4);
+	SHA1((const unsigned char*) qry, strlen(qry), (unsigned char*) k->SHA);
+}
+
+static inline ssize_t qry_val_sz(int32_t txtsz) 
+{
+	return offsetof(qry_val_t, qrytxt) + txtsz + 1;
+}
+
+#define QRY_VAL_LOCAL(local, ts, status, txt, txtsz)			\
+	ssize_t _qry_val_sz_of_ ## local = qry_val_sz(txtsz);		\
+	char _qry_val_buf_of_ ## local [_qry_val_sz_of_ ## local];	\
+	qry_val_t *local = (qry_val_t *) _qry_val_buf_of_ ## local; \
+	local->ts = ts;												\
+	local->status = status;										\
+	local->txtsz = txtsz;										\
+	memcpy(local->qrytxt, txt, txtsz);							\
+	local->qrytxt[txtsz] = 0;									\
 
 typedef struct tup_key_t {
 	char PREFIX[4]; 
@@ -48,5 +79,10 @@ typedef struct tup_key_t {
 	int32_t seq;
 } tup_key_t;
 
+static inline void tup_key_init(tup_key_t *k, const char *sha) {
+	memcpy(k->PREFIX, "TUPL", 4);
+	memcpy(k->SHA, sha, 20);
+	k->seq = 0;
+}
 
 #endif /* PGC_FDW_CACHE_H */
